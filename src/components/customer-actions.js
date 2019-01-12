816d4d4f428a646f2874009bc45402e63ef5f2bd
CesarRivasP <cesarjose58@gmail.com>
2019-01-12 17:03:24 -0400

    Creation to customerAction component

---
+++ src/components/customer-actions.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const CustomerActions = ({children}) => {
+  return (
+    <div>
+      <div className="customers-actions">
+        <div>{children}</div>
+      </div>
+
+    </div>
+  );
+}
+
+CustomerActions.proptypes = {
+  //Del tipo 'nodo' involucra todo tipo de elemento renderizable que react nos permita.
+  children: PropTypes.node.isRequired,
+};
+
+export default CustomerActions;
+
+/*
+La propiedad children hace referencia a todo el contenido dentro de un elemento (componente)
+*/
