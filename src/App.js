9dce3d445dcd9c5e8996af3749e145e4f8192e59
CesarRivasP <cesarjose58@gmail.com>
2019-01-12 14:06:20 -0400

    Added implementation of react-router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Link to="/customers">Customers</Link>
+          <br />
+          <Link to="/customers/25175344">Customers 25.175.344</Link>
+        </div>
+      </Router>
     );
   }
 }
 
 export default App;
+
+/* Al envolver la app con router se le otorga la capacidad de manejo de rutas */
