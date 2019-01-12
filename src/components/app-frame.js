3393aecd7c2b02ff7f1f21d9441682ec8419f19c
CesarRivasP <cesarjose58@gmail.com>
2019-01-12 13:02:03 -0400

    creation of component app-frame

---
+++ src/components/app-frame.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const AppFrame = ({header, body}) => {
+  return (
+    <div>
+      <div className="app-frame">
+        <AppHeader title={header} />
+      </div>
+      <div /*className="body"*/>
+        {body}
+      </div>
+      <div /*className="footer"*/>
+        Aplicacion Simple de ejemplo
+      </div>
+    </div>
+  );
+}
+
+AppFrame.propTypes = {
+  header: PropTypes.string.isRequired,
+  //body es un elemento renderisable, cualquier cosa que se pueda pasar desde react que se pueda renderizar puede ser un elemento
+  body: PropTypes.element.isRequired,
+}
+
+export default AppFrame;
+/*
+AppFrame es un contanedor que nos va a servir para no replicar codigo poniendo el titulo en cada uno de los lugares en los que
+se necesita, funciona como un marco 'plantilla?'
+*/
