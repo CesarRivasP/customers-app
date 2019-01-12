985b913bcad0fea084af2cd3a5a827f433026178
CesarRivasP <cesarjose58@gmail.com>
2019-01-12 17:35:47 -0400

    Creation to customerList component

--- src/components/customers-list.js
+++ src/components/customers-list.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CustomersListItem from './customersListItems'
 
-
-const CustomersList = ({customers}) => {
+const CustomersList = ({customers, urlPath}) => {
   return (
     <div>
       <div className="customers-list">
+        {
+          customers.map(customer => {
+            <CustomersListItem
+              key={customer.dni}
+              name={customer.name}
+              editAction={'Editar'}
+              deleteAction={'Eliminar'}
+              urlPath={urlPath}>
 
+            </CustomersListItem>
+          })
+        }
       </div>
     </div>
   );
 }
 
 CustomersList.propTypes = {
-  customers: PropTypes.array.isRequired
+  customers: PropTypes.array.isRequired,
+  urlPath: PropTypes.string.isRequired
 };
 
 export default CustomersList;
