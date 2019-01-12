f890cecf0f858f66bc012571e941544e9875425e
CesarRivasP <cesarjose58@gmail.com>
2019-01-12 17:49:32 -0400

    Creation to CustomerEdit component

---
+++ src/components/customer-edit.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const CustomerEdit = ({ name, ci, age }) => {
+  return (
+    <div>
+      <h2>Edicion del cliente</h2>
+      <h3>Name: {name}/ C.I: {ci} / Edad: {age}</h3>
+    </div>
+  );
+}
+
+CustomerEdit.propTypes = {
+  name: PropTypes.string,
+  ci: PropTypes.string,
+  age: PropTypes.number,
+};
+
+export default CustomerEdit;
