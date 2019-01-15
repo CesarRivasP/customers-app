52a4bb9a83c430e202ffa2ed152c6160bc632e8f
CesarRivasP <cesarjose58@gmail.com>
2019-01-14 23:05:53 -0400

    Creation to Customers container

---
+++ src/containers/customers-container.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import AppFrame from '../components/app-frame';
+import CustomerList from '../components/customer-list';
+import CustomerActions from '../components/customers-actions';
+
+const customers = [
+  {
+    "ci": "5606048",
+    "name": "Pedro Rivas",
+    "age": 58
+  },
+  {
+    "ci": "4432815",
+    "name": "Maria Perez Bello",
+    "age": 62
+  },
+  {
+    "ci": "24205310",
+    "name": "Cesar Rivas",
+    "age": 23
+  },
+  {
+    "ci": "19194710",
+    "name": "Andres Rivas",
+    "age": 29
+  }
+];
+
+class CustomersContainer extends Component {
+
+
+  renderBody = (customers) => (
+    <div>
+      <CustomerList customers={customers} urlPath={'customer/'} />
+      <CustomerActions>
+        <button onClick={this.handleAddNew}>Nuevo Cliente</button>
+      </CustomerActions>
+    </div>
+  );
+
+
+  render () {
+    return (
+      <div>
+        <AppFrame
+          header={'Listado de clientes'}
+          body={this.renderBody(customers)}
+        />
+      </div>
+    );
+  }
+}
+
+// CustomersContainer.propTypes = {
+//   customers: PropTypes.string.isRequired,
+//   urlPath: PropTypes.string.isRequired,
+// }
+
+export default CustomersContainer;
+
+/*
+El app-frame es para hacer mas estandar la parte visual
+El urlPath necesita el url base, por base se refiere a la base sobre la cual se va a agregar el edit o el new
+*/
