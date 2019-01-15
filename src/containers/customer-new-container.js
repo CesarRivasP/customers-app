04df2487c39f5e0c420436f7562989f2a0e37a3c
CesarRivasP <cesarjose58@gmail.com>
2019-01-15 07:56:33 -0400

    Implementation of withRouter to Customer container

---
+++ src/containers/customer-new-container.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+
+class CustomerNewContainer extends Component {
+  render () {
+    return (
+
+    )
+  }
+}
+
+// CustomerNewContainer.propTypes = {
+//
+// }
+
+export extends CustomerNewContainer;
