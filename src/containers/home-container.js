3b2569649576d915ba408175ae96b40e9f71cda8
CesarRivasP <cesarjose58@gmail.com>
2019-01-14 23:06:23 -0400

    Delete without using code

--- src/containers/home-container.js
+++ src/containers/home-container.js
@@ -37,28 +37,8 @@ class HomeContainer extends Component {
   }
 }
 
-HomeContainer.proptypes = {
-
-};
-
 export default withRouter(HomeContainer);
 /*
 - EL componente link va direccionar hacia la url de customers (donde va a estar el control del listado de clientes)
 - El appFrame nos provee un marco para la visualizacion de la aplicacion
-- Si se quiere hacer una transicion entre url se puede hacer mediante 'Link', pero si se quiere hacer Mediante
-un evento y a raiz de esa funcion ejecutar la navegacion u alguna otra condicion, se puede hacer mediante el
-elemento 'History', el cual nos permite acceder siempre y cuando el componente este dentro de el router, y dentro
-history se maneja un stack 'pila de navegacion' en la cual si se quiere navegar hacia otra pantalla, se puede
-hacer un push y de esta manera se agrega un elemento al stack y luego cuando el usuario sale de esa ventana
-se saca el elemento del stacl, de manera que se termina navegando a el elemento anterior
-en el stack.
-La propiedad history existe dentro del contenedor, a pesar de que no este declarada o importada
-El componente Route es el encargado de pasarle las variable de entorno (match, location, history) a los
-componentes indicados.
-  * Nota: para que route le pueda pasar las propiedades al componente indicado, debe ser declarado directamente
-  el componente en el atributo component de Route, si esto se hace por medio de una funcion no pasa las
-  propiedades.
-  Para que esto funcione indistintamente de la forma en la que fue declarado, se puede aplicar withRouter y dentro
-  se coloca el componente a exportar. Esto le agrega funcionalidades al componente y le agrega las tres propiedades
-  que antes solo se pasaban dependiendo de como habia sido invocado o declarado el componente
 */
