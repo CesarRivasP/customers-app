326d9f6682663d6adc23b64506f94abd771b5c0a
CesarRivasP <cesarjose58@gmail.com>
2019-01-17 10:47:54 -0400

    Implementation of reducers to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore, compose } from 'redux';
+import reducers from '../reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const reducers = (state) => state;
+// const reducers = (state) => state;
 
 export const store = createStore(reducers, {}, composeEnhancers());
