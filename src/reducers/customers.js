e5dcacd746da13b5ebc2496dc0d9fa902e94fa2e
CesarRivasP <cesarjose58@gmail.com>
2019-01-17 11:11:23 -0400

    Implementation of action in customer reducer

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -7,7 +7,11 @@ import { FETCH_CUSTOMERS } from '../constants';
 // with handleActions
 export const customers = handleActions(
   { //1 param: objto donde se mapean las pure actions del reducer
-    [FETCH_CUSTOMERS]: (state) => state
+    [FETCH_CUSTOMERS]: (state, action) =>  [...action.payload]
   },
-  {}  //2 param: default value
+  [] //2 param: array de customers
 );
+//con la accion se va a modificar el estado de tal manera que muestre los clientes que se estan cargando Dentro
+//del action creator
+//De primer parametro se hace una copia del array y en el segundo se genera un nuevo valor
+// Declarado asi, solo funciona para un reducer con un unico estado 
