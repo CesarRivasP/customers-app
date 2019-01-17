bf6d2bb05d22b668b35fd7558105e0a155c13753
CesarRivasP <cesarjose58@gmail.com>
2019-01-17 10:46:13 -0400

    Added customers

--- src/actions/fetch-customers.js
+++ src/actions/fetch-customers.js
@@ -2,5 +2,29 @@ import { createAction } from 'redux-actions';
 import { FETCH_CUSTOMERS } from '../constants';
 
 
+
+const customers = [
+  {
+    "ci": "5606048",
+    "name": "Pedro Rivas",
+    "age": 58
+  },
+  {
+    "ci": "4432815",
+    "name": "Maria Perez Bello",
+    "age": 62
+  },
+  {
+    "ci": "24205310",
+    "name": "Cesar Rivas",
+    "age": 23
+  },
+  {
+    "ci": "19194710",
+    "name": "Andres Rivas",
+    "age": 29
+  }
+];
+
 export const fetchCustomers = createAction(FETCH_CUSTOMERS);
 //Como el payload es null, no se le pasa
