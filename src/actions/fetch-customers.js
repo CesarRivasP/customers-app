6720be8d46c5cb5e6e37827627908de7769b0d74
CesarRivasP <cesarjose58@gmail.com>
2019-01-19 11:11:24 -0400

    json-server call implementation

--- src/actions/fetch-customers.js
+++ src/actions/fetch-customers.js
@@ -2,33 +2,44 @@ import { createAction } from 'redux-actions';
 import { FETCH_CUSTOMERS } from '../constants';
 
 
-
-const customers = [
-  {
-    "ci": "5606048",
-    "name": "Pedro Rivas",
-    "age": 58
-  },
-  {
-    "ci": "4432815",
-    "name": "Maria Perez Bello",
-    "age": 62
-  },
-  {
-    "ci": "24205310",
-    "name": "Cesar Rivas",
-    "age": 23
-  },
-  {
-    "ci": "19194710",
-    "name": "Andres Rivas",
-    "age": 29
-  }
-];
+// const customers = [
+//   {
+//     "ci": "5606048",
+//     "name": "Pedro Rivas",
+//     "age": 58
+//   },
+//   {
+//     "ci": "4432815",
+//     "name": "Maria Perez Bello",
+//     "age": 62
+//   },
+//   {
+//     "ci": "24205310",
+//     "name": "Cesar Rivas",
+//     "age": 23
+//   },
+//   {
+//     "ci": "19194710",
+//     "name": "Andres Rivas",
+//     "age": 29
+//   }
+// ];
 
 // export const fetchCustomers = createAction(FETCH_CUSTOMERS);
 //Como el payload es null, no se le pasa
+
+//Before
                                                   //type, payloadCreator
-export const fetchCustomers = createAction(FETCH_CUSTOMERS, () => customers); //la funcion no recibe parametros
+// export const fetchCustomers = createAction(FETCH_CUSTOMERS, () => customers); la funcion no recibe parametros
 //porque en la invocacion de la accion en el componentDidMount tampoc se le pasan parametros
 // el retorno de customers es el retorno del payloadCreator
+
+//after
+const url = 'http://localhost:3001/customers';
+
+const apiFetchCustomers = () => (
+  fetch(url)
+    .then(v => v.json())
+)
+
+export const fetchCustomers = createAction(FETCH_CUSTOMERS, apiFetchCustomers);
