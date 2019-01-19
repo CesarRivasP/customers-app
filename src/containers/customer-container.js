1f77e545e12a8f8bea6be3f9a39262044ca01ccb
CesarRivasP <cesarjose58@gmail.com>
2019-01-19 15:52:13 -0400

    Added customer container

---
+++ src/containers/customer-container.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'redux';
+import PropTypes from 'prop-types';
+
+
+class CustomerContainer extends Component {
+  render () {
+    return (
+
+    );
+  }
+}
+
+// CustomerContainer.propTypes = {
+//
+// }
+
+export default CustomerContainer
