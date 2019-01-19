cf6fb8cba5e81ab3006abc57883f0e2a493976cf
CesarRivasP <cesarjose58@gmail.com>
2019-01-19 17:48:45 -0400

    Delete without using div

--- src/components/customer-list-items.js
+++ src/components/customer-list-items.js
@@ -3,19 +3,18 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import '../index.css';
 
-const CustomerListItems = ({name, ci, editAction, deleteAction, urlPath,}) => {
+
+const CustomerListItems = ({ name, ci, editAction, deleteAction, urlPath }) => {
   return (
-    <div>
-      <div className="customers-list-item">
-        <div className="field">
-          <Link to={`${urlPath}${ci}`}>{name}</Link>
-        </div>
-        <div className="field">
-          <Link to={`${urlPath}${ci}/edit`}>{editAction}</Link>
-        </div>
-        <div className="field">
-          <Link to={`${urlPath}${ci}/delete`}>{deleteAction}</Link>
-        </div>
+    <div className="customers-list-item">
+      <div className="field">
+        <Link to={`${urlPath}${ci}`}>{name}</Link>
+      </div>
+      <div className="field">
+        <Link to={`${urlPath}${ci}/edit`}>{editAction}</Link>
+      </div>
+      <div className="field">
+        <Link to={`${urlPath}${ci}/delete`}>{deleteAction}</Link>
       </div>
     </div>
   );
