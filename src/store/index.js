1428652d2cdaae0b064e0a59511196da904abd6f
CesarRivasP <cesarjose58@gmail.com>
2019-01-19 11:12:54 -0400

    applyMiddleware, promiseMiddleware store implementation

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,13 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
 import reducers from '../reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // const reducers = (state) => state;
 
-export const store = createStore(reducers, {}, composeEnhancers());
+export const store = createStore(
+  reducers,
+  {},
+  composeEnhancers(applyMiddleware(promiseMiddleware))
+);
