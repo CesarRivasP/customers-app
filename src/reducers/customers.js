61bf94541e55d370743809f71db6cf2aaae4b605
CesarRivasP <cesarjose58@gmail.com>
2019-01-20 12:12:53 -0400

    other changes in comments

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -6,7 +6,7 @@ import { FETCH_CUSTOMERS } from '../constants';
 
 // with handleActions
 export const customers = handleActions(
-  { //1 param: objto donde se mapean las pure actions del reducer
+  { //1 param: objeto donde se mapean las pure actions del reducer
     [FETCH_CUSTOMERS]: (state, action) =>  [...action.payload]
   },
   [] //2 param: array de customers
@@ -14,4 +14,4 @@ export const customers = handleActions(
 //con la accion se va a modificar el estado de tal manera que muestre los clientes que se estan cargando Dentro
 //del action creator
 //De primer parametro se hace una copia del array y en el segundo se genera un nuevo valor
-// Declarado asi, solo funciona para un reducer con un unico estado 
+// Declarado asi, solo funciona para un reducer con un unico estado
