366ecb3fb930e03410896fb7edba81d85ae942a3
CesarRivasP <cesarjose58@gmail.com>
2019-01-20 17:56:33 -0400

    other change

--- src/components/customers-actions.js
+++ src/components/customers-actions.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../index.css';
+// import '../index.css';
 
 
-const CustomerActions = ({children}) => {
+const CustomerActions = ({ children }) => {
   return (
     <div>
       <div className="customers-actions">
