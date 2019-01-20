e37c9ed7e2c436dc1c807be3f38bfb88c3a8f2a3
CesarRivasP <cesarjose58@gmail.com>
2019-01-20 17:57:32 -0400

    Comment component without using

--- src/components/customer-list.js
+++ src/components/customer-list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import CustomersListItem from './customer-list-items'
 import '../index.css';
 
