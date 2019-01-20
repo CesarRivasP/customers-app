5bd76ff57fde4f3a1721c7899ccb5c64ede00fae
CesarRivasP <cesarjose58@gmail.com>
2019-01-20 17:56:22 -0400

    Added proptype ci

--- src/containers/customer-container.js
+++ src/containers/customer-container.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import AppFrame from '../components/app-frame.js';
+import AppFrame from '../components/app-frame';
 
 
 class CustomerContainer extends Component {
   render () {
+    const { match } = this.props;
     return (
       <div>
         <AppFrame
           header={'Cliente'}
-          body={<p>Datos del cliente</p>}>
-        </AppFrame>
+          body={<p>Datos del cliente</p>}
+        />
       </div>
     );
   }
 }
 
 CustomerContainer.propTypes = {
-
+  ci: PropTypes.string.isRequired,
+  // customer: PropTypes.object,
 }
 
 export default withRouter(connect(null, null)(CustomerContainer));
