a8cd352551234bc407c0bbae5ea9aac0af715b4a
CesarRivasP <cesarjose58@gmail.com>
2019-01-20 21:34:22 -0400

    Added selector implementation and fixed console warning

--- src/containers/customer-container.js
+++ src/containers/customer-container.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Route, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import AppFrame from '../components/app-frame';
+import { getCustomerById } from '../selectors/customers';
 
 
 class CustomerContainer extends Component {
@@ -27,7 +28,8 @@ CustomerContainer.propTypes = {
 }
 
 const mapStateToProps = (state, props) => ({
-  customer: state.customers.find((client) => client.ci === props.ci)
+  // customer: state.customers.find((client) => client.ci === props.ci)
+  customer: getCustomerById(state, props)
 })
 
 export default withRouter(connect(mapStateToProps, null)(CustomerContainer));
