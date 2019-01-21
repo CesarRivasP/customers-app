b58be4d70cf240ea732000aa46a3312ee4f781eb
CesarRivasP <cesarjose58@gmail.com>
2019-01-20 21:35:00 -0400

    Added getCustomerById selector

--- src/selectors/customers.js
+++ src/selectors/customers.js
@@ -1,2 +1,11 @@
+import { createSelector } from 'reselect';
+
+
 export const getCustomers = (state) => state.customers;
                   //retorna el array customers que pertenece al reducer
+
+export const getCustomerById = createSelector(
+  (state, props) => state.customers.find((client) => client.ci === props.ci), //esta funcion es un customer
+  // result func -- se toma el customer que retorna la funcion anterior y retorna el mismo customer
+  (customer) => customer //aqui se retorna lo mismo que viene como parametro
+)
