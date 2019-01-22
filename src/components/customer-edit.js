a5522e27c4d55b5f22bb8605bc94dac924356c53
CesarRivasP <cesarjose58@gmail.com>
2019-01-22 16:26:02 -0400

    high order component implementation

--- src/components/customer-edit.js
+++ src/components/customer-edit.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form'; //High Order Component
+import { setPropsAsInitial } from '../helpers/set-props-as-initial'; //High Order Component
 import '../index.css';
 
 const CustomerEdit = ({ name, ci, age }) => {
@@ -49,12 +50,18 @@ CustomerEdit.propTypes = {
 // mapStateToProps                                           nombre de formulario
 //after
 const CustomerEditForm = reduxForm({ form: 'customer_edit' })(CustomerEdit);
-                   // (          mapStateToProps                    )
-export default connect((state,props) => ( { initialValues: props }))(CustomerEditForm)
+// before               (             mapStateToProps               )
+//export default connect((state,props) => ( { initialValues: props }))(CustomerEditForm)
+
+export default setPropsAsInitial(CustomerEditForm); //es equivalente al uso del connect anterior
 
 /* el htmlFor es para indicar que el label trabaja en conjunto con un determinado input
 el type se refiere al tipo de contenido que va a esperar
 - initialValues: props -> props lo mapee a initial values
 - La funcion (state,props) espera un objeto que mapea el estado y las props a otro valor
  ( { initialValues: props }) -> props lo mapea a initial values
+- Si bien parece un componente conectado, no lo es porque no se esta utilizando el state, sino la
+segunda propiedad que viene en la funcion que es 'props', y se esta estableciendo a initialValues,
+es decir, que se esta transformando la propiedades que vienen al componente en las propiedades que
+se necesitan (name,ci,age)
 */
