fc9bf40165d196088cd30e7439b12dc19badb692
CesarRivasP <cesarjose58@gmail.com>
2019-01-22 20:00:19 -0400

    Added field validations

--- src/components/customer-edit.js
+++ src/components/customer-edit.js
@@ -5,6 +5,28 @@ import { reduxForm, Field } from 'redux-form'; //High Order Component
 import { setPropsAsInitial } from '../helpers/set-props-as-initial'; //High Order Component
 import '../index.css';
 
+const isRequired = (value) => (
+  //en value llega algo ?
+  !value && "Este campo es requerido"
+  //En el caso de que no haya error, retorna undefined
+  //Con undefined indica que el campo es correcto
+);
+
+const myField = ({ input, meta}) => (
+  //En input viene toda la informacion del input
+  //meta indica cuando hay errores y los distintos estados del campo
+  <div>
+    <input
+      type="text"
+      {...input} //Aqui se le estan pasando todas las propiedades de input (el campo original)
+    />
+    { meta.touched && meta.error && <span>{ meta.error }</span> }
+    {/* si viene diferente de undefined */}
+    {/* meta.touched se le agrego esta validacion para que de error solo si el campo fue tocado, y asi no da error
+      si el campo viene vacio  */}
+  </div>
+);
+
 const CustomerEdit = ({ name, ci, age }) => {
   return (
     <div>
@@ -14,19 +36,22 @@ const CustomerEdit = ({ name, ci, age }) => {
       <form action="">
         <div>
           <label htmlFor="name">Nombre</label>
-          <Field name="name" component="input" type="text">
-
-          </Field>
+          <Field
+            name="name"
+// component="input" Para usar la validacion hay que preescindir del component input en forma directa
+//Hay que generar nuestro propio componente
+            component={myField}
+            type="text"
+            validate={isRequired} //valicacion
+          />
         </div>
         <div>
           <label htmlFor="ci">C.I</label>
-          <Field name="ci" component="input" type="text">
-
-          </Field>
+          <Field name="ci" /*component="input"*/ type="text" component={myField} validate={isRequired}/>
         </div>
         <div>
           <label htmlFor="age">Edad</label>
-          <Field name="age" component="input" type="number">
+          <Field name="age" /*component="input"*/ type="number" component={myField} validate={isRequired}>
             {/* al indicar un valor numero, no permite ingresar otra cosa que no sean numeros */}
 
           </Field>
