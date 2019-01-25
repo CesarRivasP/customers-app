e0db1635292ed0b3328ed05369beecda9a8f8e5f
CesarRivasP <cesarjose58@gmail.com>
2019-01-24 21:18:51 -0400

    added enableIniatilize property

--- src/helpers/set-props-as-initial.js
+++ src/helpers/set-props-as-initial.js
@@ -6,7 +6,11 @@ export const setPropsAsInitial = (WrappedComponent) => (
   //retorna como resultado otro componente
   class extends Component {
     render () {
-      return <WrappedComponent {...this.props} initialValues={this.props}/>;
+      return <WrappedComponent
+              {...this.props}
+              initialValues={this.props}
+              // enableReinitialize 1 solucion
+            />;
     }
   }
 );
@@ -21,4 +25,12 @@ propiedad initialValues (la que necesita el componente) y esas propiedades se va
 this.props, por lo que se tiene una copia de las propiedades con initialValues
 - High Order Component se trata de una funcion que retorna un nuevo componente en base al componente
 inicial
+** AFTER **
+Este setPropsAsInitial establece las propiedades que le pasamos como inicial values.
+Esto quiere decir que se copian todas las propiedades que se estan pasando (this.props.customer) y se las
+esta copiando de initial values, pero cuando trabajo con initial values el form, solamente lo inicializa
+una vez. Es decir, que una vez que el componente fue generado, no vuelve a tomar los datos de initial
+values, entonces si no se le indica que fuerce esa accion que hace que tome los initial values (valores
+que corresponden al cliente) se queda con los valores iniciales.
+- enableReinitialize: con esta propiedad se permite tomar varias veces los valores iniciales.
 */
