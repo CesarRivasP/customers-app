14dd2c4c39c3c09f0ded9f9aebe452747be7fa73
CesarRivasP <cesarjose58@gmail.com>
2019-01-25 08:21:12 -0400

    Added action creator updateCustomer

---
+++ src/actions/update-customer.js
@@ -0,0 +1,11 @@
+import { createAction } from 'redux-actions';
+import { UPDATE_CUSTOMER } from '../constants';
+import { urlCustomers } from '../api/urls';
+import { apiPut } from '../api';
+
+
+export const updateCustomer = createAction(UPDATE_CUSTOMER,
+  (id, customer) => apiPut(urlCustomers, id, customer)()
+);
+
+// apiPut(urlCustomers, id, customer)() -> Asi se coloca para que se ejecute
