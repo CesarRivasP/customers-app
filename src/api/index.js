4817bc58826483862c9b5ac266af0f6081d13fd4
CesarRivasP <cesarjose58@gmail.com>
2019-01-25 08:20:39 -0400

    Added and implemented apiPut function

--- src/api/index.js
+++ src/api/index.js
@@ -4,3 +4,15 @@
 // )
 
 export const apiGet = (url) => () => fetch(url).then(v => v.json())
+
+export const apiPut = (url, id, obj) => () =>
+  fetch(`${url}/${id}`, {
+    method: 'PUT', // method: metodo http a utilizar
+    body: JSON.stringify(obj),  //para hacer stringify del objeto
+    headers: new Headers({'Content-type': 'application/json'})  //en base de lo que indica el json server new Headers()
+  })
+  .then(v => v.json())
+
+// el obj es el que va a venir para ser actualizado
+// url base + id del cliente -> Es una norma estandar de http rest
+//Usamos el then para transformar lo que viene en el json resultante
