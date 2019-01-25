fdea85afaab807e7834205c6377f8abdf0a48e9e
CesarRivasP <cesarjose58@gmail.com>
2019-01-25 10:52:11 -0400

    added notes

--- src/api/index.js
+++ src/api/index.js
@@ -7,10 +7,10 @@ export const apiGet = (url) => () => fetch(url).then(v => v.json())
 
 export const apiPut = (url, id, obj) => () =>
   fetch(`${url}/${id}`, {
-    method: 'PUT', // method: metodo http a utilizar
+    method: 'PUT', // method: metodo http a utilizar. Aqui se indica el metodo a ejecutar
     body: JSON.stringify(obj),  //para hacer stringify del objeto
     headers: new Headers({'Content-type': 'application/json'})  //en base de lo que indica el json server new Headers()
-  })
+  })  //el header tiene el 'application/json' para que el tipo de envio sea conocido
   .then(v => v.json())
 
 // el obj es el que va a venir para ser actualizado
