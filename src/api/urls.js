6fbba30c7088d47abd355b603c9262d4f5b5ab62
CesarRivasP <cesarjose58@gmail.com>
2019-01-27 15:31:36 -0400

    added notes and change url base

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,3 +1,3 @@
-const urlBase = 'http://localhost:3001/'
+const urlBase = 'http://localhost:3001/'; //ruta del json-server corriendo o del node server
 
 export const urlCustomers = `${urlBase}customers`;
