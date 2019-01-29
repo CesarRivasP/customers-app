5655406f53c51ebe1a859b0d959e59e8a8fcd9f3
CesarRivasP <cesarjose58@gmail.com>
2019-01-28 22:36:02 -0400

    implementation to insertCustomer action and others things

--- src/containers/new-customer-container.js
+++ src/containers/new-customer-container.js
@@ -1,32 +1,61 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
+import { SubmissionError } from 'redux-form';
+import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import AppFrame from '../components/app-frame';
 import CustomerEdit from '../components/customer-edit';
-// import CustomersActions from '../components/customers-actions'
+import { insertCustomer } from '../actions/insert-customer';
 
 
 class NewCustomerContainer extends Component {
+  //
+  // handleSubmit = (values) => {
+  //   // const { insertCustomer } = this.props;
+  //   // return this.props.insertCustomer(values)
+  //   //         .then((result) => {
+  //   //           if(result.error){
+  //   //             throw new SubmissionError(result.payload);
+  //   //           }
+  //   //         })
+  //   //         .catch((error) => {
+  //   //           throw new SubmissionError(error);
+  //   //         })
+  //   console.log(JSON.stringify(values));
+  //   console.log(`Sin metodos ${values}`);
+  //
+  //   //before return this.props.updateCustomer(id, values); aqui termina el proceso de submitting
+  //   //UpdateCustomer es la promise, por lo que hay que hacer un return de la misma
+  //
+  //   //para evaluar si el resultado de la promise viene con un error o note
+  //   this.props.insertCustomer(values)
+  //     // .then((result) => {
+  //     //   //validacion de lado del server
+  //     //   if(result.error){ //result viene con error, es decir, viene en true
+  //     //     throw new SubmissionError(result.payload); //toma el payload y lo pasa al SubmissionError
+  //     //   }
+  //     // })
+  // };
 
-  handleSubmit = () => {
-
+  handleSubmit = (values) => {
+    this.props.insertCustomer(values);
   };
 
-  handleSubmitSuccess = () => {
-
+  handleOnSubmitSuccess = () => { //cuando el submit es exitoso
+    // const { history } = this.props;
+    this.props.history.goBack();
   };
 
-  handleOnBack = () => {
-    const { history } = this.props;
-    history.goBack();
+  handleOnBack = () => {  // al cancelar
+    // const { history } = this.props;
+    this.props.history.goBack();
   };
 
   renderbody = () => {
     return (
       <CustomerEdit
         onSubmit={this.handleSubmit}
-        onSubmitSuccess={this.handleSubmitSuccess} //cuando es exitosa la subida de los datos
+        onSubmitSuccess={this.handleOnSubmitSuccess} //cuando es exitosa la subida de los datos
         onBack={this.handleOnBack}
       />
     );
@@ -43,8 +72,8 @@ class NewCustomerContainer extends Component {
   }
 }
 
-// NewCustomerContainer.propTypes = {
-//
-// }
+NewCustomerContainer.propTypes = {
+  insertCustomer: PropTypes.func.isRequired
+}
 
-export default withRouter(connect(null, null)(NewCustomerContainer));
+export default withRouter(connect(null, { insertCustomer })(NewCustomerContainer));
