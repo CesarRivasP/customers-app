6d66597f0170fedd9d282fb1dbef8ccc7a0a8b4b
CesarRivasP <cesarjose58@gmail.com>
2019-01-28 22:37:19 -0400

    Added insertCustomer action

---
+++ src/actions/insert-customer.js
@@ -0,0 +1,10 @@
+import { createAction } from 'redux-actions';
+import { INSERT_CUSTOMER } from '../constants';
+import { urlCustomers } from '../api/urls';
+import { apiPost } from '../api';
+
+export const insertCustomer = createAction(
+  INSERT_CUSTOMER,
+  // customer => console.log(customer)
+  (customer) => apiPost(urlCustomers, customer)()
+);
