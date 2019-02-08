7eab01db173c8dd0d5eefc08166ddc5bda0abe82
CesarRivasP <cesarjose58@gmail.com>
2019-02-07 20:36:40 -0400

    Added InsertCustomer reducer

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -1,17 +1,32 @@
 import { /*handleAction,*/ handleActions } from 'redux-actions';
 import { FETCH_CUSTOMERS } from '../constants';
+import { INSERT_CUSTOMER } from '../constants';
 
 // with handleAction
 // const customers = handleAction(FETCH_CUSTOMERS, (state) => state);
 
 // with handleActions
-export const customers = handleActions(
-  { //1 param: objeto donde se mapean las pure actions del reducer
-    [FETCH_CUSTOMERS]: (state, action) =>  [...action.payload]
-  },
-  [] //2 param: array de customers
-);
+//BEFORE
+// export const customers = handleActions(
+//   { //1 param: objeto donde se mapean las pure actions del reducer
+//     [FETCH_CUSTOMERS]: (state, action) =>  [...action.payload]
+//   },
+//   [] //2 param: array de customers
+// );
 //con la accion se va a modificar el estado de tal manera que muestre los clientes que se estan cargando Dentro
 //del action creator
 //De primer parametro se hace una copia del array y en el segundo se genera un nuevo valor
 // Declarado asi, solo funciona para un reducer con un unico estado
+
+//AFTER
+//Hay que incorporar el nuevo cliente al array ya existente de clientes
+ export const customers = handleActions(
+   {
+      [FETCH_CUSTOMERS]: (state, action) => [ ...action.payload ],
+      //tomamos lo que ya existia en el state -> state + el nuevo cliente
+      // que viene en el payload de la action
+      [INSERT_CUSTOMER]: (state, action) => [ ...state, action.payload ]
+      //Asi se obtiene el nuevo cliente de forma instantanea
+   },
+   []
+ )
