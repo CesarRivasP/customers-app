88a308c378b01ce08e790d711c1a6876d4ac3136
CesarRivasP <cesarjose58@gmail.com>
2019-02-10 13:23:19 -0400

    Added new material buttons and cleaning code

--- src/containers/customers-container.js
+++ src/containers/customers-container.js
@@ -2,34 +2,22 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import CustomerActions from '../components/customers-actions';
 import AppFrame from '../components/app-frame';
 import CustomerList from '../components/customer-list';
-import CustomerActions from '../components/customers-actions';
 import { fetchCustomers } from '../actions/fetch-customers';
 import { getCustomers } from '../selectors/customers';
 
-// const customers = [
-//   {
-//     "ci": "5606048",
-//     "name": "Pedro Rivas",
-//     "age": 58
-//   },
-//   {
-//     "ci": "4432815",
-//     "name": "Maria Perez Bello",
-//     "age": 62
-//   },
-//   {
-//     "ci": "24205310",
-//     "name": "Cesar Rivas",
-//     "age": 23
-//   },
-//   {
-//     "ci": "19194710",
-//     "name": "Andres Rivas",
-//     "age": 29
-//   }
-// ];
+
+const styles = theme => ({
+  input: {
+    display: 'none',
+  },
+});
+
 
 class CustomersContainer extends Component {
 
@@ -47,16 +35,25 @@ class CustomersContainer extends Component {
     this.props.history.push('/customers/new');
   }
 
-  renderBody = (customers) => (
-    <div>
-      <CustomerList
-        customers={customers}
-        urlPath={'customers/'} />
-      <CustomerActions>
-        <button onClick={this.handleAddNew}>Nuevo Cliente</button>
-      </CustomerActions>
-    </div>
-  );
+  renderBody = (customers) => {
+    const { classes } = this.props;
+    return (
+      <div>
+        <CustomerList
+          customers={customers}
+          urlPath={'customers/'} />
+        <CustomerActions>
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={this.handleAddNew}
+            className={classes.button}>
+            Nuevo Cliente
+          </Button>
+        </CustomerActions>
+      </div>
+    )
+  }
 
   render () {
     const { customers } = this.props;
@@ -74,41 +71,13 @@ class CustomersContainer extends Component {
 CustomersContainer.propTypes = {
   fetchCustomers: PropTypes.func.isRequired,
   customers: PropTypes.array.isRequired,
+  classes: PropTypes.object.isRequired,
 }
 
 //Declaracion de propiedad por defecto: nombre del componente.defaultProps
 CustomersContainer.defaultProps = {
   customers: []
-    // {
-    //   "ci": "5606048",
-    //   "name": "Pedro Rivas",
-    //   "age": 58
-    // },
-    // {
-    //   "ci": "4432815",
-    //   "name": "Maria Perez Bello",
-    //   "age": 62
-    // },
-    // {
-    //   "ci": "24205310",
-    //   "name": "Cesar Rivas",
-    //   "age": 23
-    // },
-    // {
-    //   "ci": "19194710",
-    //   "name": "Andres Rivas",
-    //   "age": 29
-    // }
 };
-
-// const mapDispatchToProps = (dispatch) => ({
-//   fetchCustomers: () => dispatch(fetchCustomers())
-// });
-
-//forma simplificada
-// const mapDispatchToProps = { fetchCustomers };
-
-// export default withRouter(connect(null, mapDispatchToProps)(CustomersContainer));
 
 const mapStateToProps = (state) => ({
   // customers: state.customers
@@ -116,8 +85,17 @@ const mapStateToProps = (state) => ({
 })
 
 // Mas simplificado aun
-export default withRouter(connect(mapStateToProps, { fetchCustomers })(CustomersContainer));
+export default withRouter(
+  compose(
+    connect(mapStateToProps, { fetchCustomers }),
+    withStyles(styles)
+)(CustomersContainer));
 
+
+// export default compose(
+//   translate('translationILMADMIN'),
+//   connect(mapStateToProps, mapDispatchToProps),
+//   reduxForm({ form: 'CentralMarket_filter' }))(CentralMarketFilter);
 /*
 El app-frame es para hacer mas estandar la parte visual
 El urlPath necesita el url base, por base se refiere a la base sobre la cual se va a agregar el edit o el new
