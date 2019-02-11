0bc9f53b70f4f272210b7451c8d12ced5c643976
CesarRivasP <cesarjose58@gmail.com>
2019-02-10 22:09:36 -0400

    Added user reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,21 @@
 import { combineReducers } from 'redux';
-import { customers } from './customers';
 import { reducer as reduxForm } from 'redux-form';
+import { customers } from './customers';
+import {
+  CUSTOMER_LIST,
+  CUSTOMER_VIEW
+} from '../constants/permissions';
 
+
+const user = (state, action) => (
+  {
+    permissions: [CUSTOMER_LIST, CUSTOMER_VIEW]
+    // permissions: []
+  }
+);
 
 export default combineReducers({
   customers,
-  form: reduxForm
+  form: reduxForm,
+  user
 });
